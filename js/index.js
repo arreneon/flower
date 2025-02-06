efaacc1a12d9142651338b912f6d0bcfee3c5a9b
arreneon <arreneonx@gmail.com>
2025-02-06 16:24:48 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'ELYS'.split('')
+const text = 'I Have Something For You'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
