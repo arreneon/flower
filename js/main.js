ef8a470c3780834ce72f5184e33bb2f116bb5bdb
arreneon <arreneonx@gmail.com>
2025-02-06 16:25:22 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('haii awak jom gb?').split('')
+    const titles = ('I Love You El').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
