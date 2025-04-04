9a62e20e760bbce7f4f4e33c48ff5c84974da48a
arreneon <arreneonx@gmail.com>
2025-04-04 23:10:24 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'ELYS'.split('')
+const text = 'ARANA'.split('')
 const text = 'I Have Something For You'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
